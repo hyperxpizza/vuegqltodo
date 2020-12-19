c1884e25cdd8fbad66700cf84c2abe984d7588c1
hyperxpizza <wojtek.frackowski@gmail.com>
2020-12-20 00:20:17 +0100

    gql schema

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,28 +1,128 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <table border="1" width="100%" style="border-collapse: collapse;">
+      <tr>
+        <th>First Name</th>
+        <th>Last Name</th>
+        <th>Email</th>
+        <th>Phone</th>
+        <th>Actions</th>
+      </tr>
+
+      <tr v-for ></tr>
+    </table>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import gql from 'graphql-tag';
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
-  }
+  data(){
+    return {
+      id: null,
+      firstName: '',
+      lastName: '',
+      email: '',
+      phone: ''
+    }
+  },
+  apollo: {
+    contacts: gql`query {
+      getAllContacts {
+        id,
+        firstName,
+        lastName,
+        email,
+        phone
+      }
+    }`
+  },
+  methods: {
+    createContact(firstName, lastName, email, phone){
+
+      const currentTimestamp = new Date("2016-07-27T07:45:00Z");
+      console.log(currentTimestamp);
+
+      console.log(`Create contact: ${email}`)
+      this.$apollo.mutate({
+        mutation: gql`mutation createContact($firstName: String!, $lastName: String!, $email: String!, $phone: String!){
+          createContact(input: {
+            firstName: $firstName,
+            lastName: $lastName,
+            email: $email,
+            phone: $phone,
+          })
+        }`,
+        variables: {
+          firstName: firstName,
+          lastName: lastName,
+          email: email,
+          phone: phone,
+        }
+      })
+      location.reload();
+    },
+    updateContact(id, firstName, lastName, email, phone){
+
+      const currentTimestamp = new Date("2016-07-27T07:45:00Z");
+
+      console.log(`Update contact: #${id}`);
+      this.$apollo.mutate({
+        mutation: gql`mutation updateContact($id: ID!, $firstName: String!, $lastName: String!, $email: String!, $phone: String!){
+          updateContact(input: {
+            id: $id,
+            firstName: $firstName,
+            lastName: $lastName,
+            email: $email,
+            phone: $phone
+          })
+        }
+        `,
+        variables: {
+          id: id,
+          firstName: firstName,
+          lastName: lastName,
+          email: email,
+          phone: phone,
+        }
+      })
+
+      location.reload();
+
+    },
+    deleteContact(id){
+      console.log(`Delete contact: # ${id}`)
+      
+      this.$apollo.mutate({
+        mutation: gql`mutation deleteContact($id: ID!){
+          deleteContact(id: $id)
+          }`,
+          variables:{
+            id: id,
+          }
+        })
+      location.reload();
+    },
+    selectContact(contact){
+      this.id = contact.id; 
+      this.firstName = contact.firstName;
+      this.lastName = contact.lastName;
+      this.email = contact.emaill;
+      this.phone = contact.phone;
+    },
+    cleanForm(){
+      this.id = null;
+      this.firstName = '';
+      this.lastName = '';
+      this.email = '';
+      this.phone = '';
+    }
+  }  
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
--- client/src/main.js
+++ client/src/main.js
@@ -1,8 +1,21 @@
 import Vue from 'vue'
 import App from './App.vue'
+import ApolloClient from "apollo-boost"
+import VueApollo from "vue-apollo"
 
 Vue.config.productionTip = false
 
+const apolloClient = new ApolloClient({
+  uri: "http://localhost:4000/graphql"
+})
+
+Vue.use(VueApollo)
+
+const apolloProvider = new VueApollo({
+  defaultClient: apolloClient,
+})
+
 new Vue({
   render: h => h(App),
+  apolloProvider,
 }).$mount('#app')
