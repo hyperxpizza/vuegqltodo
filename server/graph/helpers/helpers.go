a3d6c4bbed33b6058142debc4324ab32b6a27f8d
hyperxpizza <wojtek.frackowski@gmail.com>
2020-12-22 17:38:52 +0100

    fix helper

--- server/graph/helpers/helpers.go
+++ server/graph/helpers/helpers.go
@@ -3,7 +3,7 @@ package helpers
 import "github.com/hyperxpizza/vuegqltodo/server/graph/model"
 
 func ValidateContact(contact model.NewContact) bool {
-	if len(contact.FirstName) > 100 || len(contact.LastName) > 100 || len(contact.Email) > 100 || len(contact.Email) > 20 {
+	if len(contact.FirstName) > 100 || len(contact.LastName) > 100 || len(contact.Email) > 100 || len(contact.Phone) > 20 {
 		return false
 	}
 
