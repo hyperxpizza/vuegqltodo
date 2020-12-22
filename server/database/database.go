1166e2db0bae3cf722e95f1159055d59eee6634e
hyperxpizza <wojtek.frackowski@gmail.com>
2020-12-22 20:42:08 +0100

    connect to db function

--- server/database/database.go
+++ server/database/database.go
@@ -1,1 +1,37 @@
 package database
+
+import (
+	"database/sql"
+	"fmt"
+	"log"
+)
+
+var db *sql.DB
+
+const (
+	host = "localhost"
+	port = 5432
+)
+
+func InitDB(user, password, dbname string) {
+
+	psqlInfo := fmt.Sprintf("host=%s port=%d user=%s "+
+		"password=%s dbname=%s sslmode=disable",
+		host, port, user, password, dbname)
+
+	log.Println(psqlInfo)
+
+	database, err := sql.Open("postgres", psqlInfo)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	err = database.Ping()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	db = database
+	log.Println("Connected to the database")
+
+}
