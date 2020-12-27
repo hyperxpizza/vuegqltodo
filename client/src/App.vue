11945d14a724e3ef8d0a3913379ba582e044f276
hyperxpizza <wojtek.frackowski@gmail.com>
2020-12-27 01:04:23 +0100

    start table component

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,125 +1,41 @@
 <template>
   <div id="app">
-    <table border="1" width="100%" style="border-collapse: collapse;">
-      <tr>
-        <th>First Name</th>
-        <th>Last Name</th>
-        <th>Email</th>
-        <th>Phone</th>
-        <th>Actions</th>
-      </tr>
-
-      <tr v-for ></tr>
-    </table>
+    <Table />
   </div>
 </template>
 
 <script>
 import gql from 'graphql-tag';
+import Table from './components/Table.vue';
+
+/*
+export const CONTACTS = gql`
+  contacts{
+    id
+    firstName
+    lastName
+    email
+    phone
+  }
+`
+*/
 
 export default {
-  name: 'App',
-  data(){
-    return {
-      id: null,
-      firstName: '',
-      lastName: '',
-      email: '',
-      phone: ''
-    }
-  },
+  components: { Table },
+  name:"App",
   apollo: {
-    contacts: gql`query {
-      getAllContacts {
-        id,
-        firstName,
-        lastName,
-        email,
-        phone
-      }
-    }`
-  },
-  methods: {
-    createContact(firstName, lastName, email, phone){
-
-      const currentTimestamp = new Date("2016-07-27T07:45:00Z");
-      console.log(currentTimestamp);
-
-      console.log(`Create contact: ${email}`)
-      this.$apollo.mutate({
-        mutation: gql`mutation createContact($firstName: String!, $lastName: String!, $email: String!, $phone: String!){
-          createContact(input: {
-            firstName: $firstName,
-            lastName: $lastName,
-            email: $email,
-            phone: $phone,
-          })
-        }`,
-        variables: {
-          firstName: firstName,
-          lastName: lastName,
-          email: email,
-          phone: phone,
+    contacts: gql`
+      query {
+        contacts{
+          id
+          firstName
+          lastName
+          email
+          phone
         }
-      })
-      location.reload();
-    },
-    updateContact(id, firstName, lastName, email, phone){
-
-      const currentTimestamp = new Date("2016-07-27T07:45:00Z");
-
-      console.log(`Update contact: #${id}`);
-      this.$apollo.mutate({
-        mutation: gql`mutation updateContact($id: ID!, $firstName: String!, $lastName: String!, $email: String!, $phone: String!){
-          updateContact(input: {
-            id: $id,
-            firstName: $firstName,
-            lastName: $lastName,
-            email: $email,
-            phone: $phone
-          })
-        }
-        `,
-        variables: {
-          id: id,
-          firstName: firstName,
-          lastName: lastName,
-          email: email,
-          phone: phone,
-        }
-      })
-
-      location.reload();
-
-    },
-    deleteContact(id){
-      console.log(`Delete contact: # ${id}`)
-      
-      this.$apollo.mutate({
-        mutation: gql`mutation deleteContact($id: ID!){
-          deleteContact(id: $id)
-          }`,
-          variables:{
-            id: id,
-          }
-        })
-      location.reload();
-    },
-    selectContact(contact){
-      this.id = contact.id; 
-      this.firstName = contact.firstName;
-      this.lastName = contact.lastName;
-      this.email = contact.emaill;
-      this.phone = contact.phone;
-    },
-    cleanForm(){
-      this.id = null;
-      this.firstName = '';
-      this.lastName = '';
-      this.email = '';
-      this.phone = '';
-    }
-  }  
+      }  
+    `
+  }
 }
 </script>
 
--- client/src/main.js
+++ client/src/main.js
@@ -1,12 +1,21 @@
 import Vue from 'vue'
 import App from './App.vue'
-import ApolloClient from "apollo-boost"
+import { ApolloClient } from 'apollo-client'
 import VueApollo from "vue-apollo"
+import { createHttpLink } from 'apollo-link-http'
+import { InMemoryCache } from 'apollo-cache-inmemory'
 
 Vue.config.productionTip = false
 
+const httpLink = createHttpLink({
+  uri: "http://localhost:8888/query",
+});
+
+const cache = new InMemoryCache()
+
 const apolloClient = new ApolloClient({
-  uri: "http://localhost:4000/graphql"
+  link: httpLink,
+  cache,
 })
 
 Vue.use(VueApollo)
